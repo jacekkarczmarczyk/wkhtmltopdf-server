cd87254b577f9b451e95552c5ecd1a7ff0393a97
Jacek Karczmarczyk <jkarczm@gmail.com>
2022-04-08 23:22:55 +0200

    chore: _local.php

--- src/settings/settings.php
+++ src/settings/settings.php
@@ -4,6 +4,7 @@ use SharedTools\ArrayMerger\ArrayMerger;
 
 $instanceName = trim(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '_instance.txt') ?: '');
 $instanceFile = __DIR__ . DIRECTORY_SEPARATOR . "instance.{$instanceName}.php";
+$localFile =  __DIR__ . DIRECTORY_SEPARATOR . '_local.php';
 
 /**
  * @psalm-suppress UnresolvableInclude
@@ -12,6 +13,17 @@ $instanceSettings = require $instanceFile;
 if (!is_array($instanceSettings)) {
     throw new InvalidArgumentException();
 }
+
+if (is_file($localFile)) {
+    $localSettings = require $localFile;
+    if (!is_array($localSettings)) {
+        throw new InvalidArgumentException();
+    }
+} else {
+    $localSettings = [];
+}
+
+$instanceSettings = ArrayMerger::mergeRecursiveDistinct($instanceSettings, $localSettings);
 
 return ArrayMerger::mergeRecursiveDistinct([
     'wkhtmltopdf' => [
